7454605d93e293bdf040cf43fa1578497ed3f159
ryonakao <rodriguez.nak@gmail.com>
2016-12-23 16:43:59 +0900

    各Managerの実装

---
+++ Assets/Scripts/SalaryManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalaryManager : SingletonMonoBehaviour<SalaryManager> {
+
+	public int this_year_salary = 0;
+
+	void Start()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/SalaryUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SalaryUIManager : MonoBehaviour {
+
+	public Text thisYearSalary;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ThisYearSalary ();
+	}
+
+	void ThisYearSalary()
+	{
+		thisYearSalary.text = SalaryManager.Instance.this_year_salary.ToString () + "円";
+	}
+}
