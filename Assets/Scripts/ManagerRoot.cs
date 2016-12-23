99ae913acc23f3a2b85330b6d0c8dc3d26bb52ce
ryonakao <rodriguez.nak@gmail.com>
2016-12-23 16:27:55 +0900

    カレンダーシーンから給料シーンへの遷移完了

---
+++ Assets/Scripts/ManagerRoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerRoot : MonoBehaviour {
+
+
+	public static ManagerRoot instance;
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this.gameObject);
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : SingletonMonoBehaviour<MySceneManager> {
+
+	public void GoSalary()
+	{
+		SceneManager.LoadScene ("Salary");
+	}
+
+	public void GoCalendar()
+	{
+		SceneManager.LoadScene ("Calendar");
+	}
+}
