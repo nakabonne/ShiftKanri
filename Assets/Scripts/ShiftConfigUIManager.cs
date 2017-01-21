7cb091af42352309fac4b0af103842c89adce9a1
ryonakao <rodriguez.nak@gmail.com>
2017-01-21 21:34:11 +0900

    設定済みのバイト先を、シフト設定時に選択可能にする

--- Assets/Scripts/ShiftConfigUIManager.cs
+++ Assets/Scripts/ShiftConfigUIManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShiftConfigUIManager : MonoBehaviour {
 
-
+	public Dropdown workChoce;
+	void Start()
+	{
+		MakeWorkChoices ();
+	}
 	// Update is called once per frame
 	void Update () {
 		
@@ -13,5 +18,15 @@ public class ShiftConfigUIManager : MonoBehaviour {
 	public void BackCalendar()
 	{
 		MySceneManager.Instance.GoCalendar ();
+	}
+
+	//バイト先の選択肢を動的に生成
+	public void MakeWorkChoices()
+	{
+		for (int i = 0; i<PlayerPrefsUtility.LoadList<string> ("name").Count; i++) 
+		{
+			workChoce.options.Add (new Dropdown.OptionData{text = PlayerPrefsUtility.LoadList<string> ("name")[i]}); //一つずつボタン生成
+			workChoce.RefreshShownValue();
+		}
 	}
 }
